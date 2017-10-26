32a28a65af114495da60c759d89a8c888a13b69b
RavicharanN <ravicharan.vsp@gmail.com>
2017-10-26 23:24:43 +0530

    Complete p2p chat

--- client.py
+++ client.py
@@ -1,18 +1,28 @@
 import socket
+import time
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 12346
+port = 12348
 
+socket_list = []
+socket_list.append('recv')
 s.connect((host,port))
 
-print "Sent by server:"
-recv_data = s.recv(1024)
-print(recv_data)
-print "Type your message:"
-send_data =raw_input()
-s.send(send_data)
-s.close()
+while True:
+    if socket_list[len(socket_list)-1] == 'recv':
+        recv_data = s.recv(1024)
+        if recv_data:
+            print "Message from server:"
+            print(recv_data)
+            socket_list.remove('recv')
+            socket_list.append('send')
+    elif socket_list[len(socket_list)-1] == 'send':
+        print "Enter message:"
+        send_data = raw_input()
+        s.send(send_data)
+        socket_list.remove('send')
+        socket_list.append('recv')
 
 # while True:
 #     recv_data = s.recv(1024)
--- server.py
+++ server.py
@@ -2,22 +2,35 @@ import socket
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 12346
+port = 12348
 
-print "WAiting for connection"
+print "Waiting for connection"
 s.bind((host,port))
-s.listen(5)
+s.listen(9)
 
-client,address = s.accept()
-print "Connection Esatablished"
-print "Enter your message"
-send_data = raw_input()
-client.send(send_data)
-recv_data = client.recv(1024)
-print "Sent by client :"
-print(recv_data)
+socket_list = []
+socket_list.append('send')
+client_thread,address = s.accept()
+print("Connection established. Send a message")
 
-#
+while True:
+    if socket_list[len(socket_list)-1] == 'send':
+        print "Enter message"
+        send_data = raw_input()
+        client_thread.send(send_data)
+        socket_list.remove('send')
+        socket_list.append('recv')
+    elif socket_list[len(socket_list)-1] == 'recv':
+        recv_data = client_thread.recv(1024)
+        if recv_data:
+            print "Message fron the client:"
+            print(recv_data)
+            socket_list.remove('recv')
+            socket_list.append('send')
+
+
+
+
 # while True:
 #     (client_thread,address) = s.accept()
 #     print "Recieved from client" + address + ":"
